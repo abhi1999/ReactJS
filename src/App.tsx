bf636ce82e1befc466409c2208ec43ab520f470f
abhishek <abhi1999@gmail.com>
2018-06-07 21:20:07 -0400

    Added Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import Grid from './components/Grid';
+
+import 'ag-grid/dist/styles/ag-grid.css';
+import 'ag-grid/dist/styles/theme-fresh.css';
 
 export default class App extends React.Component{
 
     render(){
-        return <div>Hello World</div>
+        return <div>Hello World<Grid/></div>
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const webpack = require('webpack')
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const copyWebpackPlugin = require('copy-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const path = require('path');
 const rootSrc = path.resolve(__dirname, './src');
 
@@ -36,13 +38,26 @@ module.exports={
                     ignoreDiagnostics:[2339, 2307, 6143, 2551, 2345]
                 }
             },
-             {
+            {
+                test: /\.css$/,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  "css-loader"
+                ]
+              },
+            // Optionally extract less files
+            // or any other compile-to-css language
+           /* {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            },*/
+            /* {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
                   fallback: "style-loader",
                   use: ['css-loader', 'sass-loader']
                 })
-            }, 
+             },*/ 
             {   
                 test :/\.(png|jpg)$/,
                 loader: 'file-loader'
@@ -57,12 +72,21 @@ module.exports={
             filename: 'index.html',
             template: __dirname + '/src/index.html'
         })
-        /*,
-        new ExtractTextPlugin({
-            filename:'[name].css'
-        }), 
+        ,
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+          })
+        /*new ExtractTextPlugin({
+			filename: "css/[name].css?[hash]-[chunkhash]-[contenthash]-[name]",
+			disable: false,
+			allChunks: false
+		}),*/
+		/*new webpack.optimize.CommonsChunkPlugin({ name: "c", filename: "c.js" })/*, 
          new copyWebpackPlugin([
-            {from: './src/settings.js', to: 'settings.js'}
+            {from: './src/settings.js', to: 'settings.js'}y
         ]) */
     ]
 }
